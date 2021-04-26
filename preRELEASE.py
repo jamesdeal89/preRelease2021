2371086fa408c742239ac71ceecb6b6802cfe9b9
Jamie <j891319@protonmail.com>
2021-04-26 12:13:31 +0800

    added displayboard function

--- preRELEASE.py
+++ preRELEASE.py
@@ -9,13 +9,24 @@
 # one array for each time
 seatsPerCoach = 80
 price = 25
-# time, no. of coaches, tickets bought
-train1 = [0900, 6, 0]
-train2 = [1100, 6, 0]
-train3 = [1300, 6, 0]
-train4 = [1500, 6, 0]
-train1B = [1000, 6, 0]
-train2B = [1200, 6, 0]
-train3B = [1400, 6, 0]
-train4B = [1600, 8, 0]
+# time, no. of coaches, tickets bought, total money
+train1 = [900, 6, 0, 0]
+train2 = [1100, 6, 0, 0]
+train3 = [1300, 6, 0, 0]
+train4 = [1500, 6, 0, 0]
+train1B = [1000, 6, 0, 0]
+train2B = [1200, 6, 0, 0]
+train3B = [1400, 6, 0, 0]
+train4B = [1600, 8, 0, 0]
 
+def displayBoard():
+    print("train information")
+    print("time: Avaliable: Sold: MoneyTaken:")
+    print(train1[0], train1[1]*seatsPerCoach, train1[2])
+    print(train2[0], train2[1]*seatsPerCoach, train2[2])
+    print(train3[0], train3[1]*seatsPerCoach, train3[2])
+    print(train4[0], train4[1]*seatsPerCoach, train4[2])
+    
+
+
+    
