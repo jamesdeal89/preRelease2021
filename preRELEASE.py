4088eaa44784ab7e24f5629cbde4ef7d66a8f57a
Jamie <j891319@protonmail.com>
2021-04-26 11:48:27 +0800

    added files

---
+++ preRELEASE.py
@@ -0,0 +1,12 @@
+# pre-release 2021 CAIE
+
+# TASK 1
+# set up screen for the start of the day
+# screen shows: train times, number of avaliable tickets left,
+# four up, four down
+# total eight trips
+
+# one array for each time
+seatsPerCoach = 80
+# time, no. of coaches, tickets bought
+train1 = [0900, 6, 0]
