1d073c1cf72b39062c34e458c9b745fae24a501a
Jamie <j891319@protonmail.com>
2021-04-26 12:24:29 +0800

    added start of booking and adjusted board

--- preRELEASE.py
+++ preRELEASE.py
@@ -22,11 +22,25 @@ train4B = [1600, 8, 0, 0]
 def displayBoard():
     print("train information")
     print("time: Avaliable: Sold: MoneyTaken:")
-    print(train1[0], train1[1]*seatsPerCoach, train1[2])
-    print(train2[0], train2[1]*seatsPerCoach, train2[2])
-    print(train3[0], train3[1]*seatsPerCoach, train3[2])
-    print(train4[0], train4[1]*seatsPerCoach, train4[2])
+    print("trains up")
+    print(train1[0], train1[1]*seatsPerCoach, train1[2], train1[3])
+    print(train2[0], train2[1]*seatsPerCoach, train2[2], train2[3])
+    print(train3[0], train3[1]*seatsPerCoach, train3[2], train3[3])
+    print(train4[0], train4[1]*seatsPerCoach, train4[2], train4[3])
+    print("trains down")
+    print(train1B[0], train1B[1]*seatsPerCoach, train1B[2], train1B[3])
+    print(train2B[0], train2B[1]*seatsPerCoach, train2B[2], train2B[3])
+    print(train3B[0], train3B[1]*seatsPerCoach, train3B[2], train3B[3])
+    print(train4B[0], train4B[1]*seatsPerCoach, train4B[2], train4B[3])
+
+while True:
+    displayBoard()
+    print("welcome to the ticket booking system:")
+    groupSize = int(input("please enter the number of people you would like to book:"))
+    timeTrainUp = int(input("time of train up:"))
+    timeTrainDown = int(input("time of train down:"))
+    if timeTrainDown >= timeTrainUp:
+        print("please select a train down after the train up")
+        input
     
-
-
     
